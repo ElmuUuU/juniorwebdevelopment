a72ed06521adfd612a55a39dac2a259a0a3d0427
Fabian Mergner <fabian.mergner@gmail.com>
2022-01-20 14:22:04 -0800

    main

---
+++ components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="sticky top-0 z-50 items-center justify-center">
+      <div className="bg-white px-1 md:px-4 max-w-7xl mx-auto">
+        <div className="flex items-center p-6 border-b-2 border-gray-600 justify-start md:space-x-10">
+          <div className="flex w-1/2 md:space-x-10 ">
+            <div className="items-center text-center justify-center"></div>
+            <p>Test</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
--- components/Searchinput.jsx
+++ components/Searchinput.jsx
@@ -2,21 +2,7 @@ import React from "react";
 import { useState } from "react";
 
 function Searchinput() {
-  const [searchInput, setSearchInput] = useState("");
-  const resetInput = () => {
-    setSearchInput("");
-  };
-  return (
-    <div className="items-center justify-center text-center">
-      <input
-        value={searchInput}
-        onChange={(e) => setSearchInput(e.target.value)}
-        className="pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
-        type="text"
-        placeholder={"Start your search"}
-      />
-    </div>
-  );
+  return <div className="">Test</div>;
 }
 
 export default Searchinput;
