db662fe8da303c1e2d008757e3a01073cd14f5de
Fabian Mergner <fabian.mergner@gmail.com>
2022-01-21 10:18:31 -0800

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,9 +5,8 @@ function Hero() {
   return (
     <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
       <h1 className="text-6xl font-bold">Welcome to the cardboard games!</h1>
-      <input className="mt-4 py-2 p-4" placeholder="Enter the given code:" />
       <Link href="/Games">
-        <p className="mt-4 text-6xl font bold border-2 rounded-3xl pt-2 p-4 bg-gray-400 cursor-pointer">
+        <p className="mt-10 text-6xl font bold border-2 rounded-3xl pt-2 p-4 bg-yellow-300 cursor-pointer">
           Start the game
         </p>
       </Link>
