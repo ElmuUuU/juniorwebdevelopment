1b41cb1f83d869a6da03e3ca1057c8b4ba7eb17b
Fabian Mergner <fabian.mergner@gmail.com>
2022-01-21 10:56:25 -0800

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ function Hero() {
   return (
     <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
       <h1 className="text-6xl font-bold">Welcome to the cardboard games!</h1>
-      <Link href="/Gameone">
+      <Link href="/Games/Gameone">
         <p className="mt-10 text-6xl font bold border-2 rounded-3xl pt-2 p-4 bg-yellow-300 cursor-pointer">
           Start the game
         </p>
