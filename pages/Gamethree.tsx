86808f31caac297a4987d46210f4f32875982af4
Fabian Mergner <fabian.mergner@gmail.com>
2022-01-21 10:53:26 -0800

    added 3 Game pages

---
+++ pages/Gamethree.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Gamethure() {
+  return <div></div>;
+}
+
+export default Gamethure;
---
+++ pages/Gametwo.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Gamenick() {
+  return <div></div>;
+}
+
+export default Gamenick;
