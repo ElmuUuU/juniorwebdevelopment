e4a3050c91bda90eab10c6f5c2cf9feacf4e07e1
Fabian Mergner <fabian.mergner@gmail.com>
2022-01-23 13:16:34 -0800

    Update Gameone.tsx

--- pages/Games/Gameone.tsx
+++ pages/Games/Gameone.tsx
@@ -4,9 +4,18 @@ function Games() {
   return (
     <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
       <img src="/1.png" alt="" width="1280" height="720" />
-      <form action="/" method="POST">
-        <label htmlFor="name">Name</label>
-        <input id="name" type="text" autoComplete="name" required />
+      <form className="flex" action="/" method="POST">
+        <label htmlFor="name" className="mt-4">
+          Name
+        </label>
+        <input
+          className="m-4"
+          id="name"
+          type="text"
+          autoComplete="name"
+          required
+          placeholder="Put in your Name here:"
+        />
         <button type="submit">Next Game</button>
       </form>
     </div>
