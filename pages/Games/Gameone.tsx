3add77a6d0d2aab022b4bf12f9b1f909875d65c8
Fabian Mergner <fabian.mergner@gmail.com>
2022-01-23 12:05:11 -0800

    added Game one picture

--- pages/Games/Gameone.tsx
+++ pages/Games/Gameone.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Games() {
-  return <div>Test</div>;
+  return (
+    <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+      <img src="/1.png" alt="" width="1280" height="720" />
+    </div>
+  );
 }
 
 export default Games;
