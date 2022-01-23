02466e011513b09e16239dc240413cd669090132
Fabian Mergner <fabian.mergner@gmail.com>
2022-01-23 13:14:03 -0800

    Update Gameone.tsx

--- pages/Games/Gameone.tsx
+++ pages/Games/Gameone.tsx
@@ -4,10 +4,10 @@ function Games() {
   return (
     <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
       <img src="/1.png" alt="" width="1280" height="720" />
-      <form action="http://www.acme.com/register" method="POST">
-        <label for="name">Name</label>
-        <input id="name" type="text" autocomplete="name" required />
-        <button type="submit">Register</button>
+      <form action="/" method="POST">
+        <label htmlFor="name">Name</label>
+        <input id="name" type="text" autoComplete="name" required />
+        <button type="submit">Next Game</button>
       </form>
     </div>
   );
