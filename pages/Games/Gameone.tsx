7e69df793e9d8572aea71bf7585d31475100648a
Fabian Mergner <fabian.mergner@gmail.com>
2022-01-23 12:08:30 -0800

    added form to Gameone

--- pages/Games/Gameone.tsx
+++ pages/Games/Gameone.tsx
@@ -4,6 +4,11 @@ function Games() {
   return (
     <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
       <img src="/1.png" alt="" width="1280" height="720" />
+      <form action="http://www.acme.com/register" method="POST">
+        <label for="name">Name</label>
+        <input id="name" type="text" autocomplete="name" required />
+        <button type="submit">Register</button>
+      </form>
     </div>
   );
 }
