f685b0f0fc38eada6ebdbad6ada365b4d9beac69
Fabian Mergner <fabian.mergner@gmail.com>
2022-01-23 18:10:04 -0800

    Update Game2.tsx

--- pages/Games/Game2.tsx
+++ pages/Games/Game2.tsx
@@ -1,6 +1,20 @@
 import React from "react";
 
 function Gamenick() {
+  var randomGame = Math.round(Math.random() * (3 - 1) + 1);
+  var game = process.env.GAME_ID;
+
+  game = `/Games/Game${randomGame}`;
+  var previousGame = parseInt(process.env.PREVIOUS_GAME!);
+  var randomString = Math.round(Math.random() * (3 - 1) + 1);
+  function getRandomGame() {
+    if ((previousGame = 0)) {
+      randomString = Math.round(Math.random() * (3 - 2) + 2);
+      previousGame = 1;
+      game = `/Games/Game${randomString}`;
+    }
+    return game;
+  }
   return <div></div>;
 }
 
