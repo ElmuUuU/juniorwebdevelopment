c0a838f256aabb4d13ef51326a9339b6aeb1042f
Fabian Mergner <fabian.mergner@gmail.com>
2022-01-28 14:36:56 -0800

    test

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Hero from "/components/Hero";
-import '../styles/globals.css'
+import '../styles/index.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Hero from "/components/Hero";
-import styles from '../styles/Home.module.css'
+import styles from '../styles/index.css'
 
 import React from 'react';
 
