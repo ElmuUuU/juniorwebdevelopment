70c1b71e879ea95b8836bab266e83d9187442761
Fabian Mergner <fabian.mergner@gmail.com>
2022-01-28 14:03:21 -0800

    update style

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
 import Hero from "/components/Hero";
+import styles from '../styles/Home.module.css'
 
 import React from 'react';
 
 function index() {
-  return <div>
+  return <div className={styles.container}>
+  <main className={styles.main}>
   <Hero />
+  </main>
   </div>;
 }
 
