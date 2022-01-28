259b49d08837532254b5bd2d9111689929b15300
Fabian Mergner <fabian.mergner@gmail.com>
2022-01-28 13:58:48 -0800

    update

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Hero from "/components/Hero";
-import '../styles/nothing.css'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,15 +2,13 @@ import Head from "next/head";
 import Image from "next/image";
 import Hero from "/components/Hero";
 
-export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+import React from 'react';
 
-      <Hero />
-    </div>
-  );
+function index() {
+  return <div>
+  <Hero />
+  </div>;
 }
+
+export default index;
+
