4a7b39e7b97b6c2d76a8a156f092880066f9be86
Fabian Mergner <fabian.mergner@gmail.com>
2022-01-28 15:10:47 -0800

    restored

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
+import Hero from "../components/hero";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div>Yes</div>;
+  return (
+    <div className={styles.container}>
+      <main>
+        <Hero />
+      </main>
+    </div>
+  );
 }
