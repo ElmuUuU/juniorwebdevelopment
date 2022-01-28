e3ce123eeb0f9f4b768d77bf6a75637ed6436236
Fabian Mergner <fabian.mergner@gmail.com>
2022-01-28 15:06:55 -0800

    main

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,15 @@
-import Head from "next/head";
-import Image from "next/image";
 import Hero from "/components/Hero";
-import styles from "../styles/globals.css"
-
-import React from 'react';
+import styles from "../styles/globals.css";
+import React from "react";
 
 function index() {
-  return <div className={styles.container}>
-  <main >
-  <Hero />
-  </main>
-  </div>;
+  return (
+    <div>
+      <main>
+        <Hero />
+      </main>
+    </div>
+  );
 }
 
 export default index;
-
