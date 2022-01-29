2f30f4aff0621d55c78d733ad11bd73ee7067b3c
Fabian Mergner <fabian.mergner@gmail.com>
2022-01-28 18:08:42 -0800

    Update Solutions.tsx

--- pages/Solutions.tsx
+++ pages/Solutions.tsx
@@ -4,7 +4,8 @@ function Solutions() {
   return (
     <div className="mx-auto max-w-7xl items-center justify-center text-center">
       <div>
-        Game 1: <br /> Solution 1:
+        <h1 className="text-4xl">Game 1:</h1>
+        <h3>Solution 1:</h3>
         <div className="mx-auto flex justify-center p-4">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29349.591613795757!2d8.64298520852353!3d50.11534950513444!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47bd095855555555%3A0x204d96071c9db69c!2sSkyline%20Plaza%20Frankfurt!5e0!3m2!1sde!2sca!4v1643420162048!5m2!1sde!2sca"
@@ -15,21 +16,21 @@ function Solutions() {
           <div>
             <p className="h-1/3 pl-10">
               a: <br />
-              Start: Zoo Frankfurt. <br /> Finish:: Skyline Plaza Frankfurt
+              Skyline Plaza Frankfurt
             </p>
             <p className="h-1/3 pl-10">
               b: <br />
-              Start: Zoo Frankfurt. <br /> Finish:: Skyline Plaza Frankfurt
+              Start: Zoo Frankfurt. <br /> Finish: Skyline Plaza Frankfurt
             </p>
             <p className="h-1/3 pl-10">
               c: <br />
-              Start: Zoo Frankfurt. <br /> Finish:: Skyline Plaza Frankfurt
+              Start: Zoo Frankfurt. <br /> Finish: Skyline Plaza Frankfurt
             </p>
           </div>
         </div>
       </div>
       <div>
-        Game 1: <br /> Solution 2:
+        Solution 2:
         <div className="mx-auto flex justify-center p-4">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29349.591613795757!2d8.64298520852353!3d50.11534950513444!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47bd095855555555%3A0x204d96071c9db69c!2sSkyline%20Plaza%20Frankfurt!5e0!3m2!1sde!2sca!4v1643420162048!5m2!1sde!2sca"
@@ -40,20 +41,20 @@ function Solutions() {
           <div>
             <p className="h-1/3 pl-10">
               a: <br />
-              Start: Zoo Frankfurt. <br /> Finish:: Skyline Plaza Frankfurt
+              Start: Zoo Frankfurt. <br /> Finish: Skyline Plaza Frankfurt
             </p>
             <p className="h-1/3 pl-10">
               b: <br />
-              Start: Zoo Frankfurt. <br /> Finish:: Skyline Plaza Frankfurt
+              Start: Zoo Frankfurt. <br /> Finish: Skyline Plaza Frankfurt
             </p>
             <p className="h-1/3 pl-10">
               c: <br />
-              Start: Zoo Frankfurt. <br /> Finish:: Skyline Plaza Frankfurt
+              Start: Zoo Frankfurt. <br /> Finish: Skyline Plaza Frankfurt
             </p>
           </div>
         </div>
         <div>
-          Game 1: <br /> Solution 3:
+          Solution 3:
           <div className="mx-auto flex justify-center p-4">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29349.591613795757!2d8.64298520852353!3d50.11534950513444!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47bd095855555555%3A0x204d96071c9db69c!2sSkyline%20Plaza%20Frankfurt!5e0!3m2!1sde!2sca!4v1643420162048!5m2!1sde!2sca"
@@ -64,21 +65,22 @@ function Solutions() {
             <div>
               <p className="h-1/3 pl-10">
                 a: <br />
-                Start: Zoo Frankfurt. <br /> Finish:: Skyline Plaza Frankfurt
+                Start: Zoo Frankfurt. <br /> Finish: Skyline Plaza Frankfurt
               </p>
               <p className="h-1/3 pl-10">
                 b: <br />
-                Start: Zoo Frankfurt. <br /> Finish:: Skyline Plaza Frankfurt
+                Start: Zoo Frankfurt. <br /> Finish: Skyline Plaza Frankfurt
               </p>
               <p className="h-1/3 pl-10">
                 c: <br />
-                Start: Zoo Frankfurt. <br /> Finish:: Skyline Plaza Frankfurt
+                Start: Zoo Frankfurt. <br /> Finish: Skyline Plaza Frankfurt
               </p>
             </div>
           </div>
         </div>
       </div>
-      <div>Game 2:</div>
+      <div className="text-4xl">Game 2:</div>
+      <div className="text-4xl">Game 3:</div>
     </div>
   )
 }
