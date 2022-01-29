3fcae936cd37ff4967395cbd46e4753d619f063d
Fabian Mergner <fabian.mergner@gmail.com>
2022-01-28 17:56:20 -0800

    main

--- pages/Solutions.tsx
+++ pages/Solutions.tsx
@@ -1,12 +1,86 @@
-import React from "react";
-import GoogleMaps from "../components/GoogleMaps";
+import React from 'react'
 
 function Solutions() {
   return (
-    <div className="max-w-7xl">
-      <GoogleMaps />
+    <div className="mx-auto max-w-7xl items-center justify-center text-center">
+      <div>
+        Game 1: <br /> Solution 1:
+        <div className="mx-auto flex justify-center p-4">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29349.591613795757!2d8.64298520852353!3d50.11534950513444!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47bd095855555555%3A0x204d96071c9db69c!2sSkyline%20Plaza%20Frankfurt!5e0!3m2!1sde!2sca!4v1643420162048!5m2!1sde!2sca"
+            width="600"
+            height="450"
+            loading="lazy"
+          ></iframe>
+          <div>
+            <p className="h-1/3 pl-10">
+              a: <br />
+              Start: Zoo Frankfurt. <br /> Finish:: Skyline Plaza Frankfurt
+            </p>
+            <p className="h-1/3 pl-10">
+              b: <br />
+              Start: Zoo Frankfurt. <br /> Finish:: Skyline Plaza Frankfurt
+            </p>
+            <p className="h-1/3 pl-10">
+              c: <br />
+              Start: Zoo Frankfurt. <br /> Finish:: Skyline Plaza Frankfurt
+            </p>
+          </div>
+        </div>
+      </div>
+      <div>
+        Game 1: <br /> Solution 2:
+        <div className="mx-auto flex justify-center p-4">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29349.591613795757!2d8.64298520852353!3d50.11534950513444!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47bd095855555555%3A0x204d96071c9db69c!2sSkyline%20Plaza%20Frankfurt!5e0!3m2!1sde!2sca!4v1643420162048!5m2!1sde!2sca"
+            width="600"
+            height="450"
+            loading="lazy"
+          ></iframe>
+          <div>
+            <p className="h-1/3 pl-10">
+              a: <br />
+              Start: Zoo Frankfurt. <br /> Finish:: Skyline Plaza Frankfurt
+            </p>
+            <p className="h-1/3 pl-10">
+              b: <br />
+              Start: Zoo Frankfurt. <br /> Finish:: Skyline Plaza Frankfurt
+            </p>
+            <p className="h-1/3 pl-10">
+              c: <br />
+              Start: Zoo Frankfurt. <br /> Finish:: Skyline Plaza Frankfurt
+            </p>
+          </div>
+        </div>
+        <div>
+          Game 1: <br /> Solution 3:
+          <div className="mx-auto flex justify-center p-4">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29349.591613795757!2d8.64298520852353!3d50.11534950513444!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47bd095855555555%3A0x204d96071c9db69c!2sSkyline%20Plaza%20Frankfurt!5e0!3m2!1sde!2sca!4v1643420162048!5m2!1sde!2sca"
+              width="600"
+              height="450"
+              loading="lazy"
+            ></iframe>
+            <div>
+              <p className="h-1/3 pl-10">
+                a: <br />
+                Start: Zoo Frankfurt. <br /> Finish:: Skyline Plaza Frankfurt
+              </p>
+              <p className="h-1/3 pl-10">
+                b: <br />
+                Start: Zoo Frankfurt. <br /> Finish:: Skyline Plaza Frankfurt
+              </p>
+              <p className="h-1/3 pl-10">
+                c: <br />
+                Start: Zoo Frankfurt. <br /> Finish:: Skyline Plaza Frankfurt
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>Game 2:</div>
     </div>
-  );
+  )
 }
 
-export default Solutions;
+export default Solutions
