aa7ca8897894a2922eaa1de92ff5edabacebaf9b
Fabian Mergner <fabian.mergner@gmail.com>
2022-01-30 11:30:56 -0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useState } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [clickAmount, setClickAmount] = useState(0)
+  const increment = () => setClickAmount((amount) => amount + 1)
+
+  return (
+    <Component {...pageProps} clickAmount={clickAmount} increment={increment} />
+  )
 }
 
 export default MyApp
