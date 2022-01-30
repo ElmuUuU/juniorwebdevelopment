4db6db168715f4efc0efb94d68b449c3f2f239d6
Fabian Mergner <fabian.mergner@gmail.com>
2022-01-29 18:02:13 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Hero from '../components/Hero'
 
 export default function Home() {
+  var lives = 3
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
